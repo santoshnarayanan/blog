f34fabffde9b4cd1a6aac767f38de7a45788e07c
santosh <santosh33@gmail.com>
2022-09-12 19:08:28 +0530

    Fetching and Rendering Posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 const App = () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Post</h1>
+      <PostList />
     </div>
   );
 };
